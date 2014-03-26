98e74876fb2574dd9e7f7b03197aa9c56d64be7d
JohnsonChan <itc0z0sti@gmail.com>
2014-03-26 20:36:51 +0800

    init

--- src/com/czs/mygame/GameLayer.java
+++ src/com/czs/mygame/GameLayer.java
@@ -29,7 +29,7 @@ public class GameLayer extends CCLayer {
     private CCSprite upBarTwo;
     private CCSprite downBarOne;
     private CCSprite downBarTwo;
-    
+
     private float height = 0.0f;
     private float weight = 0.0f;
     private float scaleX = 0.0f;
@@ -37,64 +37,60 @@ public class GameLayer extends CCLayer {
     private float barHeight = 0.0f;    // 中心点，往上高多少，往下低多少
     private float barWeight = 0.0f;   // 障碍的宽
     
-    private float speep = 7.0f;
+    private float Y = 0;
 
-
-//    private CCAnimation ccAnimation = CCAnimation.animation("", 0.2f);
+    private float speep = 7.0f;   // 速度
 
     public GameLayer(Context context) {
         this.context = context;
-        CGSize cgSize    = CCDirector.sharedDirector().winSize();
+        CGSize cgSize = CCDirector.sharedDirector().winSize();
         height = cgSize.height;
         weight = cgSize.width;
-        barHeight = height /10;
+        barHeight = height / 10;
         LogUtil.d("height:" + height);
         LogUtil.d("weight:" + weight);
 
         // 背景
-        background = CCSprite.sprite("bg.png");     
+        background = CCSprite.sprite("bg.png");
         this.addChild(background);
-        background.setAnchorPoint(0, 0);   //默认情况下 anchorPoint 为（ 0.5,0.5 ），即贴图对象 的中心位置对应着节点背景对象 的左下角；而当 anchorPoint 为（0,0 ），即贴图对象 的左下角对应着节点背景对象 的左下角
+        background.setAnchorPoint(0, 0);   // 默认情况下 anchorPoint 为（ 0.5,0.5 ），即贴图对象 的中心位置对应着节点背景对象 的左下角；而当 anchorPoint 为（0,0 ），即贴图对象 的左下角对应着节点背景对象 的左下角
         background.setPosition(0, 0);
-        scaleX=  weight/background.getContentSize().getWidth();
+        scaleX = weight / background.getContentSize().getWidth();
         background.setScaleX(scaleX);
-        scaleY = height/background.getContentSize().getHeight();
+        scaleY = height / background.getContentSize().getHeight();
         background.setScaleY(scaleY);
-        
-        // 地板
-      
-        
+
+        // 第一条障碍物上
         upBarOne = CCSprite.sprite("up_bar.png");
         upBarOne.setScaleX(scaleX);
         upBarOne.setScaleY(scaleY);
         barWeight = upBarOne.getContentSize().getWidth() * scaleX;
         this.addChild(upBarOne);
-        upBarOne.setAnchorPoint(0,0);
-        upBarOne.setPosition(weight, height/2+barHeight);
-
+        upBarOne.setAnchorPoint(0, 0);
+        upBarOne.setPosition(weight, height / 2 + barHeight);
+        // 第一条障碍物下
         downBarOne = CCSprite.sprite("down_bar.png");
         downBarOne.setScaleX(scaleX);
         downBarOne.setScaleY(scaleY);
         this.addChild(downBarOne);
-        downBarOne.setAnchorPoint(0,0);
-        downBarOne.setPosition(weight, 0-barHeight);
-//        
-//        
+        downBarOne.setAnchorPoint(0, 0);
+        downBarOne.setPosition(weight, 0 - barHeight);
+        // 第二条障碍物上
         upBarTwo = CCSprite.sprite("up_bar.png");
         upBarTwo.setScaleX(scaleX);
         upBarTwo.setScaleY(scaleY);
         this.addChild(upBarTwo);
-        upBarTwo.setAnchorPoint(0,0);  
-        upBarTwo.setPosition(weight*3/2+barWeight/2, height/2+barHeight);
-        
+        upBarTwo.setAnchorPoint(0, 0);
+        upBarTwo.setPosition(weight * 3 / 2 + barWeight / 2, height / 2 + barHeight);
+        // 第二条障碍物下
         downBarTwo = CCSprite.sprite("down_bar.png");
         downBarTwo.setScaleX(scaleX);
         downBarTwo.setScaleY(scaleY);
         this.addChild(downBarTwo);
-        downBarTwo.setAnchorPoint(0,0);
-        downBarTwo.setPosition(weight*3/2+barWeight/2, 0-barHeight);
-        
-      
+        downBarTwo.setAnchorPoint(0, 0);
+        downBarTwo.setPosition(weight * 3 / 2 + barWeight / 2, 0 - barHeight);
+
+        // 地板
         floorOne = CCSprite.sprite("ground.png");
         this.addChild(floorOne);
         floorTwo = CCSprite.sprite("ground.png");
@@ -107,57 +103,89 @@ public class GameLayer extends CCLayer {
         floorOne.setScaleX(scaleX);
         floorTwo.setScaleY(scaleY);
         floorTwo.setScaleX(scaleX);
-        
-        
-//
-//
-       
-//        
-//      
+
+
         bird = CCSprite.sprite("loading_01.png");
-        CCAnimation animation = CCAnimation.animation("dance", 0.05f);
-        for (int i = 1; i < 12; i++) {
-            animation.addFrame(new CCFormatter().format("loading_%02d.png", i));
-            LogUtil.d(new CCFormatter().format("loading_%02d.png", i));
-        }
-
-        CCIntervalAction action = CCAnimate.action(animation);
-
         this.addChild(bird);
+        bird.setScale(1.5f);
+        Y = bird.getPosition().y;
 
-        CGSize s = CCDirector.sharedDirector().winSize();
 
-        bird.setPosition(CGPoint.make(s.width / 3, s.height / 2));
-        CCRepeatForever ccRepeatForever = CCRepeatForever.action(action);
-        bird.runAction(ccRepeatForever);
+
+        bird.runAction(birdAction(1));
+
+        bird.setPosition(CGPoint.make(weight / 3, height / 2));
         this.setIsTouchEnabled(true);
-//
-//
-//      
+
+        // 循环刷新处理
         this.schedule("refreshBar", 0.01f);
         this.schedule("refreshFloor", 0.01f);
+        this.schedule("birdState", 0.1f);
     }
-    
+
+    public void birdState(float dt) {
+      if (Y > bird.getPosition().y)
+      {
+         bird.runAction(birdAction(2));
+      }
+      if (Y < bird.getPosition().y)
+      {
+          bird.runAction(birdAction(0));
+      }
+      if (Y == bird.getPosition().y)
+      {
+          bird.runAction(birdAction(1));
+      }
+      Y = bird.getPosition().y;
+    }
+
+    // 小鸟动作
+    private CCRepeatForever birdAction(int index) {
+        String picName = null;
+        switch (index) {
+            case 0:
+                picName = "birdUp";
+                break;
+            case 1:
+                picName = "birdNor";
+                break;
+            case 2:
+                picName = "birdDown";
+                break;
+            case 3:
+                picName = "birdDie";
+                break;
+        }
+        CCAnimation animation = CCAnimation.animation("dance", 0.01f);
+        for (int i = 1; i < 4; i++) {
+            animation.addFrame(new CCFormatter().format(picName + "%d.png", i));
+
+        }
+        CCIntervalAction action = CCAnimate.action(animation);
+        CCRepeatForever ccRepeatForever = CCRepeatForever.action(action);
+        return ccRepeatForever;
+    }
+
     // 刷新障碍
     public void refreshBar(float dt) {
-    	upBarOne.setPosition(upBarOne.getPosition().x - speep, upBarOne.getPosition().y);
-    	downBarOne.setPosition(downBarOne.getPosition().x - speep, downBarOne.getPosition().y);
+        upBarOne.setPosition(upBarOne.getPosition().x - speep, upBarOne.getPosition().y);
+        downBarOne.setPosition(downBarOne.getPosition().x - speep, downBarOne.getPosition().y);
         if (upBarOne.getPosition().x < -barWeight) {
-        	Random random = new Random();
-        	int i = random.nextInt((int)barHeight);
-        	upBarOne.setPosition(weight, height/2 + barHeight - i);
-        	LogUtil.d("czs:"+ (height/2 + barHeight + i));
-        	downBarOne.setPosition(weight, 0 - barHeight - i);
+            Random random = new Random();
+            int i = random.nextInt((int) barHeight);
+            upBarOne.setPosition(weight, height / 2 + barHeight - i);
+            LogUtil.d("czs:" + (height / 2 + barHeight + i));
+            downBarOne.setPosition(weight, 0 - barHeight - i);
         }
-    	
-        
+
+
         upBarTwo.setPosition(upBarTwo.getPosition().x - speep, upBarTwo.getPosition().y);
         downBarTwo.setPosition(downBarTwo.getPosition().x - speep, downBarTwo.getPosition().y);
         if (upBarTwo.getPosition().x < -barWeight) {
-        	Random random = new Random();
-        	int i = random.nextInt((int)barHeight);
-        	upBarTwo.setPosition(weight, height/2 + barHeight + i);
-        	downBarTwo.setPosition(weight, 0 - barHeight + i);
+            Random random = new Random();
+            int i = random.nextInt((int) barHeight);
+            upBarTwo.setPosition(weight, height / 2 + barHeight + i);
+            downBarTwo.setPosition(weight, 0 - barHeight + i);
         }
     }
 
@@ -179,7 +207,7 @@ public class GameLayer extends CCLayer {
         SoundEngine.sharedEngine().playEffect(context, R.raw.sfx_wing);
         return CCTouchDispatcher.kEventHandled;
     }
-    
+
     @Override
     public boolean ccTouchesMoved(MotionEvent event) {
         CGPoint point = CGPoint.ccp(100, 320);
@@ -187,7 +215,7 @@ public class GameLayer extends CCLayer {
         bird.runAction(jumpTo);
         return super.ccTouchesMoved(event);
     }
-    
+
     // 进入处理
     @Override
     public void onEnter() {
